f991d6377bda097239e17d1516d8a5a06e02b2ca
Matt J Raibert and Cassie Moy <mattraibert@positiondev.com and cassiemoy@positiondev.com>
2017-10-30 19:36:16 -0400

    Query for the list of positions

--- cactus.rb
+++ cactus.rb
@@ -3,15 +3,6 @@ $stdout.sync = true
 require 'rest-client'
 require 'uri'
 
-positions = ["Old Man Jr", "Wilt CU", "Overview", "Sag CU", "SW", "West", "Spike-Organ-Clarence",
-             "Spike middle", "Opuntia Village", "E barard1", "Mammilaria", "Quimilo",
-             "E barard2", "Stetsona", "MF middle", "Santa Rita", "Silver Torch", "PonyTailTop",
-             "North", "MF Top", "Red beards", "PolygonaGetto", "Soft Serve", "Spike top",
-             "Pachypodium", "Golden Barrels", "Sag_CU-focus", "OldMan", "Hook", "Sag arm3 CU",
-             "Scarpa-Polygona", "Sputnik", "Barrel town", "E variegated", "O variegated",
-             "Ruffles", "SpineyGuy_CU", "Bishops Cap", "South", "Sag upper", "Bullet",
-             "OctoberPoly", "PolygonaGroup", "Silver Dollar", "Twins", "Menorah Jr", "Aloe tree"]
-
 class Position
   TIME_PATTERN = "%Y-%m-%d-%H-%M-%S"
   WAIT_SECONDS = 2
@@ -33,6 +24,13 @@ class Position
     RestClient.get("http://www.raibert.com:8085/axis-cgi/com/ptz.cgi?gotoserverpresetname=#{URI.escape(@position)}&camera=1&speed=100")
   end
 
+  def self.all
+    presets = RestClient.get("http://www.raibert.com:8085/axis-cgi/com/ptz.cgi?query=presetposcam&camera=1")
+    presets.body.split("\r\n").drop(1).map do |preset|
+      Position.new(preset.split("=").last)
+    end
+  end
+
   def get_image!
     response = RestClient.get("http://www.raibert.com:8085/axis-cgi/jpg/image.cgi?camera=1")
 
@@ -46,19 +44,18 @@ class Position
 
   def filepath
     filename = "#{@position.gsub(" ", "_")}-#{@created_at.strftime(TIME_PATTERN)}.jpg"
-    filepath = File.join("/code/images", filename)
+    filepath = File.join("images", filename)
   end
 end
 
-positions.each do |position|
-  p = Position.new(position)
+Position.all.each do |position|
   10.times do
     begin
-      if !p.exists?
-        p.snap!
+      if !position.exists?
+        position.snap!
       end
     rescue Errno::ECONNREFUSED => e
-      puts "Failed to connect. Retrying... \n"    
+      puts "Failed to connect. Retrying... \n"
     end
   end
 end
