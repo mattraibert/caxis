41e56fcccce9cbc9f53fceed0a639848139eea82
Matt J Raibert <mattraibert@positiondev.com>
2018-03-11 13:57:50 -0400

    Label multiple images at once

--- tf_files/rm_small.rb
+++
@@ -1,3 +0,0 @@
-`rsync -av  ~/Google\ Drive/Training\ Cactus/ ./data`
-`find . -iname Icon* | xargs rm`
-not_enough_photos = Dir[?*].map {|d| [d,Dir.chdir(d) { Dir[?*].count }] }.select {|k,v| v < 20 }.map(&:first)
